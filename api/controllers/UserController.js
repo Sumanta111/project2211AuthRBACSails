1ea4088e035bb7bac74a86b5182604878928684b
= <=>
2018-11-23 16:11:48 +0530

    added winston and logging

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -9,6 +9,9 @@ const jwt = require('jwt-simple');
 module.exports = {
     //open permission
     register : async function(req,res){
+        if(!req.body.name || !req.body.password || !req.body.email){
+            return res.status(422).send({msg : 'Please provide all the required parameters'});
+        }
         let data = {
             name : req.body.name,
             email : req.body.email,
@@ -24,12 +27,15 @@ module.exports = {
     },
     //open permission
     login : async function(req,res){
+        if(!req.body.password || !req.body.email){
+            return res.status(422).send({msg : 'Please provide all the required parameters'});
+        }
         const user = await User.findOne({
             email : req.body.email
         }).intercept((err)=>{
             if(err) return res.serverError(err);
         });
-
+        //sails.log(user);
         if(!user){
             res.status(404).send({msg : 'Authentication failed . User not found'});
         }else{
@@ -37,6 +43,9 @@ module.exports = {
                 if (isMatch && !err) {
                     // if user is found and password is right create a token
                     var token = jwt.encode(user, 'MySecretForEncode');
+                    sails.log.info('information realated logs');
+                    sails.log.debug('debugging related logs');
+                    sails.log.error('Error related logs');
                     // return the information including token as JSON
                     res.ok({
                         ...user,
@@ -59,6 +68,9 @@ module.exports = {
     },
     //can view only his profile not other profiles ...Admin can view every profiles
     findSingleUser : async function(req,res){
+        if(!req.param('id')){
+            return res.status(422).send({msg : 'Please provide all the required parameters'});
+        }
         let user = await User.findOne({id : req.param('id')}).intercept(err=>{
             if(err) return res.serverError(err);
         })
--- config/log.js
+++ config/log.js
@@ -9,6 +9,98 @@
  * For more information on the Sails logger, check out:
  * https://sailsjs.com/docs/concepts/logging
  */
+var winston = require('winston');
+//let customLogger;
+
+function customTimestamp() {
+  var wholedate, time, date;
+  wholedate = new Date();
+  time = wholedate.toLocaleTimeString();
+  date = wholedate.toLocaleDateString();
+  wholedate = date + ' ' + time;
+  return wholedate;
+}
+
+
+if (process.env.NODE_ENV === 'development') {
+  var customLogger = winston.createLogger({
+    timestamp: function () {
+      return customTimestamp();
+    },
+    level:'silly',
+    format: winston.format.combine(
+      winston.format.timestamp({
+        format: 'YYYY-MM-DD HH:mm:ss'
+      }),
+      winston.format.colorize({ all: true }),
+      winston.format.json()
+    ),
+    transports: [
+      new winston.transports.File({
+        filename: 'logs/errors.log',
+        level: 'error'
+      }),
+      new winston.transports.File({
+        filename: 'logs/warn.log',
+        level: 'warn'
+      }),
+      new winston.transports.File({
+        filename: 'logs/info.log',
+        level: 'info'
+      }),
+      new winston.transports.File({
+        filename: 'logs/verbose.log',
+        level: 'verbose'
+      }),
+      new winston.transports.File({
+        filename: 'logs/debug.log',
+        level: 'debug'
+      }),
+      new winston.transports.File({
+        filename: 'logs/silly.log',
+        level: 'silly'
+      }),
+    ]
+  });
+  //If we're not in production then log to the `console` with the format:
+  //`${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+  customLogger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }));
+}
+
+if (process.env.NODE_ENV === 'production') {
+  var customLogger = winston.createLogger({
+    timestamp: function () {
+      return customTimestamp();
+    },
+    level:'info',
+    format: winston.format.combine(
+      winston.format.timestamp({
+        format: 'YYYY-MM-DD HH:mm:ss'
+      }),
+      winston.format.colorize({ all: true }),
+      winston.format.json()
+    ),
+    transports: [
+      new winston.transports.File({
+        filename: 'logs/errors.log',
+        level: 'error'
+      }),
+      new winston.transports.File({
+        filename: 'logs/warn.log',
+        level: 'warn'
+      }),
+      new winston.transports.File({
+        filename: 'logs/info.log',
+        level: 'info'
+      }),
+    ]
+  });
+  customLogger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }));
+}
 
 module.exports.log = {
 
@@ -25,5 +117,12 @@ module.exports.log = {
   ***************************************************************************/
 
   // level: 'info'
+
+  // Pass in our custom logger, and pass all log levels through.
+  custom: customLogger,
+  //level: 'info',
+
+  // Disable captain's log so it doesn't prefix or stringify our meta data.
+  inspect: false
 
 };
