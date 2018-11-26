ca8abaf2441456804c7c28ec6661ec6dd3c77ed7
= <=>
2018-11-26 10:54:05 +0530

    added file upload with async and await

--- api/controllers/UploadFilesController.js
+++ api/controllers/UploadFilesController.js
@@ -9,17 +9,18 @@ module.exports = {
   imageUpload : async function(req,res){
       try{
         var uploadedFiles = await req.file('avatar').upload({
+            maxBytes: 10000000,
             dirname: require('path').resolve(sails.config.appPath, 'assets/images/avatar')
         });
       }catch(err){
         return res.serverError(err);
       }
-      if(uploadedFiles){
-          console.log(uploadedFiles._files[0].stream.fd);
+      if(uploadedFiles._files.length > 0){
+          var img_name = uploadedFiles._files[0].stream.fd.split('/').splice(-1)[0];
           try{
             var savetodb = await UploadFiles.create({
-                name : 'x',
-                path : uploadedFiles._files[0].stream.fd
+                name : img_name,
+                path : `${req.protocol}://${req.headers.host}/images/avatar/${img_name}`
               }).fetch().intercept(err=>{
                 if(err) return res.serverError(err);
               });
@@ -28,14 +29,13 @@ module.exports = {
           }
           if(savetodb){
               res.json({
+                  msg : 'Uploaded file successfully',
                   file : savetodb
               })
           }
+      }else{
+        return res.badRequest('No file was uploaded');
       }
-    // sails.log(uploadedFiles._files.length);
-    // res.json({
-    //     file : 'uploaded successfully'
-    // })
   }
 
 };
